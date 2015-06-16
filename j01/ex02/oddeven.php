c20c6eeae5d3bb09650a4ba5c706c10a9c47b30e
philippe <philippe@MacBook-Pro-de-macbook.local>
2015-06-16 13:49:55 +0200

    in progress

--- j01/ex02/oddeven.php
+++ j01/ex02/oddeven.php
@@ -1,4 +1,12 @@
 #!/usr/bin/php
 <?PHP
-
+$myvar = 1;
+if (is_numeric($myvar))
+	echo "True\n";
+else
+	echo "False\n";
+if ($var_check%2)
+	echo "Le chiffre $myvar"." est Impair\n";
+else
+	echo "Le chiffre $myvar"." est pair\n";
 ?>
---
+++ j01/ex04/aff_param.php
@@ -0,0 +1,8 @@
+#!/usr/bin/php
+<?PHP
+foreach ($argv as $elem)
+{
+	if (!)
+	echo "$elem\n";
+}
+?>
